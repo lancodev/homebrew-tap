49635aee0eb7dca7d27f5fcc9e034bcbad02c877
Kyle Anderson <kyle@creative2.com>
2026-07-22 11:06:56 -0400

    Bumps ray version

--- Casks/ray.rb
+++ Casks/ray.rb
@@ -1,5 +1,5 @@
 cask "ray" do
-  version "3.2.5"
+  version "3.2.10"
   sha256 "1f7b3b170fb16da009732193b41d738f077e3f54e5528d7e621bf4c8abbfef29"
 
   url "https://ray-app.s3.eu-west-1.amazonaws.com/ray-app-updates-v3/stable/ray-#{version}-latest-darwin-arm64.dmg"
