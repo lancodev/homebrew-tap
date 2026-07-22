631846f63d47731fb9085be6acf82ef666daa277
Kyle Anderson <kyle@creative2.com>
2026-07-22 11:30:41 -0400

    strata 0.7.0

--- Formula/strata.rb
+++ Formula/strata.rb
@@ -1,8 +1,8 @@
 class Strata < Formula
   desc "TUI database client for SQLite, MySQL/MariaDB, and PostgreSQL"
   homepage "https://github.com/lancodev/strata"
-  url "https://github.com/lancodev/strata/archive/refs/tags/v0.6.0.tar.gz"
-  sha256 "f17aaa3973817f620439b67dbf1254b2ac750c7a2feb76dd4737aec8698952e5"
+  url "https://github.com/lancodev/strata/archive/refs/tags/v0.7.0.tar.gz"
+  sha256 "682102552308029154a84789cd57b5a4579ebbf0fbad1768ceb7f46d07bb61f0"
   license "MIT"
   depends_on "go" => :build
 
