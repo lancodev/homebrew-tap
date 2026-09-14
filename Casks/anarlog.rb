8d404674303ad1800c41df485f5e0035eda03912
Kyle Anderson <kyle@creative2.com>
2026-09-14 08:52:25 -0400

    anarlog: remove deprecated `verified` url parameter

--- Casks/anarlog.rb
+++ Casks/anarlog.rb
@@ -5,8 +5,7 @@ cask "anarlog" do
   sha256 arm:   "b23cfb69cf815b6fe4428c28314905205fd7680ebeed055acf266b705b349405",
          intel: "7317f3d9a8624028ab3e59e12687cc8f0e195524378ad9ba1b15e6c17b018b2d"
 
-  url "https://github.com/fastrepl/anarlog/releases/download/desktop_v#{version}/hyprnote-macos-#{arch}.dmg",
-      verified: "github.com/fastrepl/"
+  url "https://github.com/fastrepl/anarlog/releases/download/desktop_v#{version}/hyprnote-macos-#{arch}.dmg"
   name "Anarlog"
   desc "Open-source, local-first, privacy-first AI meeting notetaker"
   homepage "https://anarlog.so/"
